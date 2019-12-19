e1513db500d37b74ab72c0c28e1c4013ec8e1686
DiNikOs <demon22odn08@rambler.ru>
2019-12-20 01:59:35 +0300

    HW8 Docker + Heroku + postgresQLAdmin

--- heroku-create-apps.bat
+++ heroku-create-apps.bat
@@ -1,7 +1,12 @@
 #heroku-create-apps.bat
-#heroku apps:create g15-shop-admin-ui
+heroku apps:create g15-shop-admin-ui
+
 heroku apps:create g15-shop-ui
+
 heroku addons:create heroku-postgresql:hobby-dev --app g15-shop-admin-ui
+
 heroku addons:create heroku-postgresql:hobby-dev --app g15-shop-ui
+
 heroku config:set JDBC_DRIVER_CLASS=org.postgresql.Driver HIBERNATE_DIALECT=org.hibernate.dialect.PostgreSQLDialect --app g15-shop-admin-ui
-heroku config:set JDBC_DRIVER_CLASS=org.postgresql.Driver HIBERNATE_DIALECT=org.hibernate.dialect.PostgreSQLDialect --app g15-shop-ui
+
+heroku config:set JDBC_DRIVER_CLASS=org.postgresql.Driver HIBdockerERNATE_DIALECT=org.hibernate.dialect.PostgreSQLDialect --app g15-shop-ui
--- heroku-deploy-apps.bat
+++ heroku-deploy-apps.bat
@@ -1,3 +1,4 @@
 #heroku-deploy-apps.bat
 heroku container:release web --app=g15-shop-admin-ui
+
 heroku container:release web --app=g15-shop-ui
